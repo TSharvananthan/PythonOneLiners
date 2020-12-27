850dae6db39388fdee7a59d42c26cab075bdb2d2
TSharvananthan <>
2020-12-27 16:48:49 -0500

    small fixes to chess 960 board

--- generate_chess_960_board.py
+++ generate_chess_960_board.py
@@ -1,2 +1,1 @@
 print("{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n{6}\n{7}\n{8}".format(" ".join(__import__("random").sample(["R", "N", "B", "K", "Q", "B", "N", "R"], 8)), 'P '*8, "* "*8, "* "*8, "* "*8, "* "*8, "* "*8, 'P '*8, " ".join(__import__("random").sample(["R", "N", "B", "K", "Q", "B", "N", "R"], 8))))
-
